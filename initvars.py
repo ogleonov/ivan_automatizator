5558117a83f4200ddf616c743736a55a87b9b463
ogleonov <oleglionaff@mail.ru>
2019-11-12 01:50:35 +0300

    add_functionality

--- initvars.py
+++ initvars.py
@@ -4,3 +4,5 @@ dir = "C:/Users/Oleg/PycharmProjects/ImageProcessing/ivan_automatizator/"
 docPath = os.path.join(dir, "document.docx")
 dirIn = os.path.join(dir, "in/")
 dirOut = os.path.join(dir, "out/")
+height = 600
+widht = 800
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from Scaler import scale_image
 import os
+
 from docx import Document
+
+from Scaler import scale_image
+from initvars import dir
 from initvars import dirIn
 from initvars import dirOut
-from initvars import dir
 from initvars import docPath
+from initvars import height
+from initvars import widht
 
 
 def loadImagesToOutDir(dir):
     if not os.path.exists(dirOut):
         os.mkdir(dirOut)
     for name in os.listdir(dirIn):
-        scale_image(os.path.join(dirIn, name), os.path.join(dirOut, name), 800, 600)
+        scale_image(os.path.join(dirIn, name), os.path.join(dirOut, name), widht, height)
 
 
 if __name__ == '__main__':
     loadImagesToOutDir(dir)
     document = Document()
-    
+
     for name in os.listdir(dirOut):
         p = document.add_paragraph(name, 'List Bullet')
         p = p.insert_paragraph_before('')
