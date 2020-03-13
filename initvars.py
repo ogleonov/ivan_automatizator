898b4c8662f6ffc8b31f45e8efa8a111f3f67d5e
ivan-kapkan <57454256+ivan-kapkan@users.noreply.github.com>
2020-03-13 10:14:16 +0000

    Update initvars.py

--- initvars.py
+++ initvars.py
@@ -1,8 +1,16 @@
 import os
 
-dir = "C:/Users/Oleg/PycharmProjects/ImageProcessing/ivan_automatizator/"
-docPath = os.path.join(dir, "document.docx")
-dirIn = os.path.join(dir, "in/")
+dir = "C:/Users/ivan.vasilyev/Documents/Python/python_projects/automation/"
+docPath = os.path.join(dir, "docs/document.docx")
+dirIn = os.path.join(dir,"in/")
 dirOut = os.path.join(dir, "out/")
-height = 600
-widht = 800
+
+hcm = 12.67   #12.67; 7.69; BK - 30.0
+wcm = 9.5 #9.5; 10.27; BK - 12.0
+
+pix = 37.89
+
+height = hcm*pix
+widht = wcm*pix
+
+angle = 90
