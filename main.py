49fc77608738ed303d8ff527881e7cfc6f607d2d
ivan-kapkan <57454256+ivan-kapkan@users.noreply.github.com>
2020-03-13 10:16:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
 from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from caption import Figure
 
 from Scaler import scale_image
 from initvars import dir
@@ -10,6 +13,17 @@ from initvars import docPath
 from initvars import height
 from initvars import widht
 
+'To delete the files in dirOut'
+folder = dirOut
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder,filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def loadImagesToOutDir(dir):
     if not os.path.exists(dirOut):
@@ -22,10 +36,32 @@ if __name__ == '__main__':
     loadImagesToOutDir(dir)
     document = Document()
 
+    style = document.styles['Normal']
+    font = style.font
+    font.name = 'Calibri'
+
     for name in os.listdir(dirOut):
-        p = document.add_paragraph(name, 'List Bullet')
-        p = p.insert_paragraph_before('')
+
+        paragraph_format = document.styles['Normal'].paragraph_format
+        paragraph_format.space_after = Pt(0)
+        paragraph_format.line_spacing = Pt(0)
+
+        p = document.add_paragraph('')
+        p.alignment = WD_ALIGN_PARAGRAPH.CENTER
         r = p.add_run()
         r.add_picture(os.path.join(dirOut, name))
-        p = p.insert_paragraph_before(name, 'Heading 1')
+
+        font.bold = True
+        plate = document.add_paragraph('Plate ',style='Normal')
+        Figure(plate)
+        plate.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        d = plate.add_run(' - Lift # - Grid #')
+
+        c = document.add_paragraph()
+        cc = c.add_run('[caption]')
+        cc.bold = False
+        c.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        p = document.add_paragraph('')
+
     document.save(docPath)
+    os.startfile(docPath)
